48f119c903d0ac1c4e5f576f9eea06c010fc1068
sly1061101 <sly1061101@gmail.com>
2017-10-28 13:11:00 -0500

    Come up with a testing heuristic
    Come up with a testing heuristic which takes the winning status into consideration

--- Src/Heuristics/Heuristic_Test.cpp
+++ Src/Heuristics/Heuristic_Test.cpp
@@ -9,12 +9,23 @@
 #include "Heuristic_Test.hpp"
 
 float heu::Heuristic_Test::operator()(board::GameBoard board, player::PlayerSide side){
+    float Player0Score = 0;
+    float Player1Score = 0;
+    
+    Player0Score = 2 * ( board.GetStatusNum(board::Player0) );
+    Player1Score = 2 * ( board.GetStatusNum(board::Player1) );
+    
+    if(player::Player::DetectWinner(board, false) == player::Player0)
+        Player0Score += 99999999;
+    else if(player::Player::DetectWinner(board, false) == player::Player1)
+        Player1Score += 99999999;
+    
     float evaluation = 0;
     if(side == player::Player0){
-        evaluation = 2 * ( 1 ) + ( rand() / double(RAND_MAX) );
+        evaluation = Player0Score + (100 - Player1Score) + ( rand() / double(RAND_MAX) );
     }
     else if(side == player::Player1){
-        evaluation = 2 * ( 1 ) + ( rand() / double(RAND_MAX) );
+        evaluation = Player1Score + (100 - Player0Score) + ( rand() / double(RAND_MAX) );
     }
     return evaluation;
 }
